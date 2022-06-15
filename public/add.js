195170a49dfd736639cae1c18b9f18bef7d0cf9d
Thet-Rain <92264416+Thet-Rain@users.noreply.github.com>
2022-06-15 05:12:22 -0400

    V1 Demo App Draft Without GeoLocator Complete
    V1 Demo App Draft Without GeoLocator Complete

---
+++ public/add.js
@@ -0,0 +1,45 @@
+const gasLeakForm = document.getElementById('gasLeak-form');
+const gasLeakReporter = document.getElementById('gasLeak-reporter');
+const gasLeakAddress = document.getElementById('gasLeak-address');
+const gasLeakDescription = document.getElementById('gasLeak-description');
+
+//Send POST to API to add gas report
+async function addGasReport(e)
+{
+    e.preventDefault();
+
+    if(gasLeakReporter.value =='' || gasLeakAddress.value ==''|| gasLeakDescription == '')
+    {
+        alert('Please fill in fields');
+    }
+
+    const sendBody = {
+        witnessName: gasLeakReporter.value,
+        address: gasLeakAddress.value,
+        description: gasLeakDescription.value
+    }
+
+    try {
+        const res = await fetch('/api/v1/stores', {
+           method: 'POST',
+           headers: {
+            'Content-Type': 'application/json'
+           } ,
+           body: JSON.stringify(sendBody)
+        });
+
+        if(res.status === 400){
+            throw Error('Gas Report already Exist!')
+        }
+
+        alert('GasLeak Has Been Reported! Thank you for your contribution to the society!');
+        window.location.href = '/index.html';
+
+    } catch (err) {
+        alert(err);
+        return;
+    }
+
+}
+
+gasLeakForm.addEventListener('submit', addGasReport);
--- public/index.js
+++ public/index.js
@@ -73,21 +73,13 @@
      '<div id="content">' +
      '<div id="siteNotice">' +
      "</div>" +
-     '<h1 id="firstHeading" class="firstHeading">Gas Leak Report</h1>' +
+     '<h4 id="firstHeading" class="firstHeading">Gas Leak Report</h1>' +
      '<div id="bodyContent">' +
-     "<p><b>GasLeak Report Description</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-     "sandstone rock formation in the southern part of the " +
-     "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-     "south west of the nearest large town, Alice Springs; 450&#160;km " +
-     "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-     "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-     "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-     "Aboriginal people of the area. It has many springs, waterholes, " +
-     "rock caves and ancient paintings. Uluru is listed as a World " +
-     "Heritage Site.</p>" +
-     '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-     "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-     "(last visited June 22, 2009).</p>" +
+     '<p><b>GasLeak is reported by '+ data.data[i].witnessName +'</b>' +
+     '<h4>Gas Leak Description </h1>'+
+     data.data[i].description   +
+     '<h4>Gas Leak TimeStamp </h1>'+
+     data.data[i].createdAt   +
      "</div>" +
      "</div>";
    const infowindow = new google.maps.InfoWindow({
@@ -99,7 +91,7 @@
           icon: icon,
           map: map,
           label: {
-            text: "Reported By Jane Doe",
+            text: "Reported By " + data.data[i].witnessName,
             color: "#F00",
             textWeight: "bold"
           },
@@ -126,7 +118,7 @@
 
     
      
-
+    
 
     
     
