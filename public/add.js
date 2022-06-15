5e14a1ff414044e1f8adb728bdbaa5948fbe99bc
Thet-Rain <92264416+Thet-Rain@users.noreply.github.com>
2022-06-15 08:38:41 -0400

    V1

--- public/add.js
+++ public/add.js
@@ -12,33 +12,36 @@ async function addGasReport(e)
     {
         alert('Please fill in fields');
     }
-
-    const sendBody = {
-        witnessName: gasLeakReporter.value,
-        address: gasLeakAddress.value,
-        description: gasLeakDescription.value
-    }
-
-    try {
-        const res = await fetch('/api/v1/stores', {
-           method: 'POST',
-           headers: {
-            'Content-Type': 'application/json'
-           } ,
-           body: JSON.stringify(sendBody)
-        });
-
-        if(res.status === 400){
-            throw Error('Gas Report already Exist!')
+    else
+    {
+        const sendBody = {
+            witnessName: gasLeakReporter.value,
+            address: gasLeakAddress.value,
+            description: gasLeakDescription.value
         }
-
-        alert('GasLeak Has Been Reported! Thank you for your contribution to the society!');
-        window.location.href = '/index.html';
-
-    } catch (err) {
-        alert(err);
-        return;
+    
+        try {
+            const res = await fetch('/api/v1/stores', {
+               method: 'POST',
+               headers: {
+                'Content-Type': 'application/json'
+               } ,
+               body: JSON.stringify(sendBody)
+            });
+    
+            if(res.status === 400){
+                throw Error('Gas Report already Exist!')
+            }
+    
+            alert('GasLeak Has Been Reported! Thank you for your contribution to the society!');
+            window.location.href = '/index.html';
+    
+        } catch (err) {
+            alert(err);
+            return;
+        }
     }
+
 
 }
 
